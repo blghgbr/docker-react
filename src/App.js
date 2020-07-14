8f708032014bd047973ff31eb22d7d32b2a56c94
Balogh Gabor <nemigazhmindenfoglalt@gmail.com>
2020-07-14 22:53:22 +0200

    text update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. {TCI}
+          Edit <code>src/App.js</code> and save to reload. <div>Last Update in text...</div>
         </p>
         <a
           className="App-link"
